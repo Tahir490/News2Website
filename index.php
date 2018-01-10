7f038d2e6841e749e292edc04e1cf7a87d3c5020
najeebfazli <35286731+najeebfazli@users.noreply.github.com>
2018-01-10 23:05:47 +0500

    index
    index.php page fornt page

---
+++ index.php
@@ -0,0 +1,191 @@
+<html>
+<head>
+<title>Daily Himalaya</title>
+<link rel="stylesheet" type="text/css" href="style1.css">
+<link rel="stylesheet" type="text/css" href="css/chromestyle.css" />
+<script type="text/javascript" src="">
+
+</script>
+</head>
+<body>
+<div id="head">
+<img src="images1/logo.jpg" />
+</div>
+<div id="header">
+ <div id="ads">
+  <table align="center">
+    <tr>
+	   <td>
+	     <a href="" target="_blank"><img src="" alt="image" width="317" height="60"  border="0" /></a>
+	   </td>
+	   <td>
+	     <a href="" target="_blank"><img src="" alt="image" width="317" height="60"  border="0" /></a>
+	   </td>
+	   <td>
+	      <a href="" target="_blank"><img src="" alt="image" width="317" height="60" border="0" /></a>
+	   </td>
+	</tr>
+  </table>
+ </div>
+</div>
+<div id="breakingnews">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td width="91%" valign="middle">
+	<div align="right">
+      <marquee truespeed="truespeed" direction="right" scrolldelay="200" >
+ <a href="javascript: void(0)" 
+   onclick="" class="style1"><strong></strong></a>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="style2">|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+       </marquee>
+    </div>
+	</td>
+    <td width="9%"><div class="news"></div></td>
+  </tr>
+</table>
+</div>
+<div id="menubar">
+   <div class="chromestyle" id="chromemenu">
+    <table width="96%" border="0" align="right" cellpadding="2" cellspacing="2">
+	  <tr>
+	    <td>
+		  <form name="form" id="form">
+		   <div align="right">
+		    <span class="style3">Dated:</span>
+              <select name="jumpMenu" id="jumpMenu" onChange="MM_jumpMenu('parent',this,0)">
+			  <option value="">1/01/2018</option>
+			  <option value="">2/01/2018</option>
+			  <option value="">3/01/2018</option>
+			  </select>
+		   </div>
+		  </form>
+		</td>
+		<td width="7%"><img src="images1/shumara.jpg" /></td>
+        <td width="11%">
+		  <a href="#"><img src="images1/video-gallery.jpg" width="56" height="25" border="0" /></a>
+		</td>
+        <td width="11%">
+		  <a href="#" rel="dropmenu3"><img src="images1/pages.jpg" border=0/></a>
+		</td>
+        <td width="10%">
+		  <a href="#" rel="dropmenu2"><img src="images1/editions.jpg" border=0/></a>
+		</td>
+        <td width="9%">
+		<a href="#" rel="dropmenu1"><img src="images1/btn1.jpg" border=0/></a>
+		</td>
+	    <td width="11%">
+		<a href="index.php"><img src="images1/frontpage.jpg" width="60" height="25" border="0" /></a>
+		</td>
+	  </tr>
+	</table>
+   </div>
+   <!-- pages -->
+<div id="dropmenu3" class="dropmenudiv"  style="text-align:left;">
+    <a href="index.php?pid=1&eid=1&nid=1&tnid=7858&date=" ><strong>1-Front</strong></a>
+      <a href="index.php?pid=2&eid=1&nid=1&tnid=7858&date=" ><strong>2-News</strong></a>
+      <a href="index.php?pid=3&eid=1&nid=1&tnid=7858&date=" ><strong>3-News</strong></a>
+      <a href="index.php?pid=4&eid=1&nid=1&tnid=7858&date=" ><strong>4-Editorial</strong></a>
+      <a href="index.php?pid=5&eid=1&nid=1&tnid=7858&date=" ><strong>5-Sports</strong></a>
+      <a href="index.php?pid=6&eid=1&nid=1&tnid=7858&date=" ><strong>6-Classified</strong></a>
+      <a href="index.php?pid=7&eid=1&nid=1&tnid=7858&date=" ><strong>7- Baqia</strong></a>
+      <a href="index.php?pid=8&eid=1&nid=1&tnid=7858&date=" ><strong>8-Back Page</strong></a>
+      <a href="index.php?pid=9&eid=1&nid=1&tnid=7858&date=" ><strong>Sup1</strong></a>
+      <a href="index.php?pid=10&eid=1&nid=1&tnid=7858&date=" ><strong>Sup2</strong></a>
+      <a href="index.php?pid=11&eid=1&nid=1&tnid=7858&date=" ><strong>Sup3</strong></a>
+      <a href="index.php?pid=12&eid=1&nid=1&tnid=7858&date=" ><strong>Sup4</strong></a>
+  </div>
+  <!-- editions -->
+<div id="dropmenu2" class="dropmenudiv" style="text-align:left;">
+    <a  href="index.php?eid=1&nid=1"   >Gilgit</a>
+        <a  href="#"   >Place Name</a>
+        <a  href="#"   >Place Name</a>
+    </div>
+	<!-- newspapers -->                                                   
+<div id="dropmenu1" class="dropmenudiv" >
+      <a href="index.php?nid=1"><img src="images/Daily K2.jpg" border="0" /></a>
+        <a href="#"><img src="" border="0" /></a>
+        <a href="#"><img src="" border="0" /></a>
+    </div>
+	   <script type="text/javascript">
+      cssdropdown.startchrome("chromemenu")
+      </script>
+
+</div>
+ <div id="maincontent">
+  <img src="newspaper/1.jpg" width="980px" id="gallerya" name="gallerya"/>
+</div>
+ <div id="header">
+<div id="ads">
+<table align="center">
+<tr>
+<td>
+<a href="#" target="_blank"><img src="banners/footer 1.gif" width="317" height="60" border="0"/></a></td>
+<td>
+<a href="#" target="_blank"><img src="banners/2.gif" width="317" height="60" border="0"/></a></td>
+<td>
+<a href="#" target="_blank"><img src="banners/3.gif" width="317" height="60" border="0" /></a></td>
+</tr>
+</table>
+</div>
+</div>
+<div id="thumbs">
+<table width="100%">
+<tr>
+<td width="5%" valign="top">
+<a href="/index.php?pageNum_thumbs=0&totalRows_thumbs=8">
+<img src="images1/left.jpg" border="0" title="prev" /></a>
+</td>
+<td width="91%" valign="middle">
+<a href="index.php?pid=1&eid=1&nid=1&tnid=7858&date="><img src="newspaper/thumbs/1.jpg" alt="Page # 1" width="82" height="82" border="2" style="border-color: #0099CC"/></a>&nbsp;&nbsp;
+<a href=""><img src="newspaper/thumbs/2.jpg" alt="Page # 2" width="82" height="82" border="2" style="border-color: #0099CC"/></a>&nbsp;&nbsp;
+<a href=""><img src="newspaper/thumbs/3.jpg" alt="Page # 3" width="82" height="82" border="2" style="border-color: #0099CC"/></a>&nbsp;&nbsp;
+<a href=""><img src="newspaper/thumbs/4.jpg" alt="Page # 4" width="82" height="82" border="2" style="border-color: #0099CC"/></a>&nbsp;&nbsp;
+<a href=""><img src="newspaper/thumbs/5.jpg" alt="Page # 5" width="82" height="82" border="2" style="border-color: #0099CC"/></a>&nbsp;&nbsp;
+<a href=""><img src="newspaper/thumbs/6.jpg" alt="Page # 6" width="82" height="82" border="2" style="border-color: #0099CC"/></a>&nbsp;&nbsp;
+<a href=""><img src="newspaper/thumbs/7.jpg" alt="Page # 7" width="82" height="82" border="2" style="border-color: #0099CC"/></a>&nbsp;&nbsp;
+<a href=""><img src="newspaper/thumbs/8.jpg" alt="Page # 8" width="82" height="82" border="2" style="border-color: #0099CC"/></a>&nbsp;&nbsp;
+</td>
+<td width="4%" align="right" valign="top">
+<a href=""><img src="images1/right.jpg" border="0" title="next" /></a></td>
+</tr>
+</table>
+</div>
+<div id="footer">
+<div class="copyrights">
+  <div align="center"><span class="style31">Daily Himalaya</span></div>
+</div>
+<div id="footernav">
+  
+  <div align="center">
+    <table width="100%" height="51" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td height="33">
+            <div align="right">
+			    <a href="index.php"><strong>Home</strong></a>  
+                <a href="#"><strong>About Us</strong></a>  
+                <a href="#"><strong>Developer</strong></a>  
+                <a href="#"><strong>Contact Us</strong></a>
+			 </div>
+		 </td>
+        </tr>
+        <tr>
+             <td valign="bottom">
+			   <div align="left" class="style5">
+                 <div align="right">
+			       Copyrights &copy;2010 www.dailyHimalya.com, All rights reserved. 
+			     </div>
+              </div>
+		    </td>
+        </tr>
+      </table>
+      <br />
+  </div>
+</div>  
+<div class="icons">
+<a href="#"><img src="images1/icon1.jpg" border="0" class="iconimage" title="share"/></a> 
+<a href="#"><img src="images1/icon2.jpg" border="0" class="iconimage"  title="twitter"/></a> 
+<a href="#"><img src="images1/icon3.jpg" border="0" class="iconimage"  title="facebook"/> </a>
+<a href="#"><img src="images1/icon4.jpg" border="0" class="iconimage"   title="youtube"/></a></div>
+</div>
+</body>
+</html>
